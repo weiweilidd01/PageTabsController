1edf478576876ad63034bdf60ca6c54be3747e5d
weiweili <weiweili@hk01.com>
2018-12-20 13:03:37 +0800

    更新

--- Demo/PageTabsControllerDemo/PageTabsController/PageSegmentView.swift
+++ Demo/PageTabsControllerDemo/PageTabsController/PageSegmentView.swift
@@ -111,7 +111,7 @@ public class PageSegmentView: UIView {
         addSubview(collectionView)
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
